2cb753e3335030c949ae1c69453f453c53990b34
abu-3abed <abu.3abed.2010@gmail.com>
2018-03-06 02:04:00 +0300

    2 cats added.

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
-var elem = document.getElementById("#my-img");
+window.onload = function() {
 
-elem.addEventListener('click', function(){
+    var elem = document.getElementById("my-img");
 
-}, false);
+    var count = 0;
+
+    elem.addEventListener('click', function() {
+        document.getElementById("counter").innerHTML = "Click count: " + ++count;
+    }, false);
+
+    var elem2 = document.getElementById("my-img2");
+
+    var count2 = 0;
+
+    elem2.addEventListener('click', function() {
+        document.getElementById("counter2").innerHTML = "Click count: " + ++count2;
+    }, false);
+
+    // clear the screen for testing
+    document.body.innerHTML = '';
+    document.body.style.background = "white";
+
+    var nums = [1, 2, 3];
+
+    // Let's loop over the numbers in our array
+    for (var i = 0; i < nums.length; i++) {
+
+        // This is the number we're on...
+        var num = nums[i];
+
+        // We're creating a DOM element for the number
+        var elem = document.createElement('div');
+        elem.textContent = num;
+
+        // ... and when we click, alert the value of `num`
+        elem.addEventListener('click', function() {
+            alert(num);
+        });
+
+        // finally, let's add this element to the document
+        document.body.appendChild(elem);
+    };
+}
